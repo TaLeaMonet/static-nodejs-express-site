b362cc29e97ae3e0faf92e75dfe48f3b1491e2cd
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-07-17 19:38:28 -0700

    Iterate over projects in projects.json file.

--- static_node_and_express_site-v1/routes/index.js
+++ static_node_and_express_site-v1/routes/index.js
@@ -7,17 +7,16 @@ router.get('/', (req, res) => {
     res.locals.projects = data.projects;
     res.render('index');
 });
+
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
-/** 
- * Continue working on dynamic routes based on each project id 
- */
 router.get('/project/:id', (req, res) => {
     res.render('project', {
-        projects: data.projects[req.params.id] 
+        projects: data.projects[req.params.id], 
     });
+         
 });
 
 module.exports = router; 
